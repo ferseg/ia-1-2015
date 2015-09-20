ed8710afaf182e06f381458d63f1a4b71418c78b
Fernando Segovia <fernandosegovia25@gmail.com>
2015-09-20 16:56:10 -0600

    algoritmo para obtener las transformaciones de las filas

--- src/algorithm/algorithm.py
+++ src/algorithm/algorithm.py
@@ -1,9 +1,13 @@
+from copy import copy, deepcopy
+
 P = 0
 Y = 1
 O = 2
 G = 3
 H = 4
 E = -1
+
+MAXIMUN_NUMBER_OF_ROWS = 5
 
 example_mat = [[E, H, E, E],
 			   [P, Y, P, O],
@@ -15,10 +19,39 @@ NOTCH_SYMBOL = 4
 UP = -1
 DOWN = 1
 
-
 notch_row = 0
 notch_column = 0
 babylon_tower = []
+
+# Contains all the states that are open
+open_states = []
+
+# Contains all the states that are close
+closed_states = []
+
+def generate_states_moving_rows(matrix):
+    result = []
+    for current_row_index in range(0, MAXIMUN_NUMBER_OF_ROWS):
+        # The resulting matrix when the transformation "right" is applied
+        tmp_matrix_right = deepcopy(matrix)
+        # The resulting matrix when the transformation "right" is applied
+        tmp_matrix_left = deepcopy(matrix)
+
+        # >> Begin right transformation <<
+        current_row = matrix[current_row_index]
+        # resulting_row for the right transformation of the current row
+        resulting_row = move_row_to_right(current_row)
+        tmp_matrix_right[current_row_index] = resulting_row
+        result += [tmp_matrix_right]
+        # >> End right transformation <<
+
+        # >> Begin left transformation <<
+        # resulting_row for the right transformation of the current row
+        resulting_row = move_row_to_left(current_row)
+        tmp_matrix_left[current_row_index] = resulting_row
+        result += [tmp_matrix_left]
+        # >> End left transformation <<
+    return result
 
 
 def init_babylon_tower(p_initial_state):
@@ -41,13 +74,13 @@ def move_notch(p_movements,p_direction):
     global babylon_tower
     direction_counter = p_direction
     while p_movements != 0:
-        temp = babylon_tower[notch_row+p_direction][notch_column]
+        temp = babylon_tower[notch_row + p_direction][notch_column]
         babylon_tower[notch_row][notch_column] = temp
-        babylon_tower[notch_row+p_direction][notch_column] = NOTCH_SYMBOL
+        babylon_tower[notch_row + p_direction][notch_column] = NOTCH_SYMBOL
         notch_row += p_direction
         p_movements -= 1
     
-def print_Data():
+def print_data():
     print (notch_row)
     print (notch_column)
     print (babylon_tower)
@@ -70,4 +103,7 @@ def move_row_to_left(pRow):
 # Testing
 print(move_row_to_right(example_mat[1]))
 print(move_row_to_left(example_mat[1]))
+print(init_babylon_tower(example_mat))
+print(generate_states_moving_rows(example_mat))
+print_data()
 
