c819f381aa315abd210a5c132dc3d41bb13356e8
Fernando Segovia <fernandosegovia25@gmail.com>
2015-09-20 16:01:45 -0600

    Constants

--- src/algorithm/algorithm.py
+++ src/algorithm/algorithm.py
@@ -1,14 +1,15 @@
-# 0 = P
-# 1 = Y
-# 2 = O
-# 3 = G
-# 4 = H
-# -1 = Empty
-example_mat = [[-1,4,-1,-1],
-			   [0, 1, 0, 2],
-			   [2, 3, 2, 3],
-			   [3, 0, 3, 1],
-			   [1, 2, 1, 0]]
+P = 0
+Y = 1
+O = 2
+G = 3
+H = 4
+E = -1
+
+example_mat = [[E, H, E, E],
+			   [P, Y, P, O],
+			   [O, G, O, G],
+			   [G, P, G, Y],
+			   [Y, O, Y, P]]
 
 # Moves the elements of the row one space to the right
 def move_row_to_right(pRow):
@@ -24,6 +25,7 @@ def move_row_to_left(pRow):
 	result += [pRow[0]]
 	return result
 
+# Testing
 print(move_row_to_right(example_mat[1]))
 print(move_row_to_left(example_mat[1]))
 
